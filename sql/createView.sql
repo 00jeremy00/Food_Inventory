c45c0ffb2b11bfd3ead448ab1fde414b03d3f948
Jeremy <j191@umbc.edu>
2026-03-10 23:24:07 -0400

    created queries and updated views

--- sql/createView.sql
+++ sql/createView.sql
@@ -1,13 +1,16 @@
 CREATE OR REPLACE VIEW ReceiveInfo AS
 SELECT 
-t.transaction_num as 'transaction number',
-t.transaction_type as "transaction type",
-i.internal_num as "item number",
-i.internal_name as 'item',
-t.quantity as 'quantity',
-i.internal_unit as 'unit',
-t.invoice_num as 'invoice number',
-m.manager_name as 'manager'
+t.transaction_num,
+t.transaction_date,
+t.transaction_type,
+i.internal_num,
+i.internal_name,
+t.quantity,
+i.internal_unit ,
+t.invoice_num,
+m.manager_name,
+m.manager_num
+
 FROM Item as i JOIN InventoryTransaction as t
 ON i.internal_num = t.internal_num
 JOIN Managers as m
@@ -17,14 +20,16 @@ WHERE t.transaction_type = 'RECEIVE';
 
 CREATE OR REPLACE VIEW  WasteAdjustInfo AS
 SELECT
-t.transaction_num as 'transaction number',
-t.transaction_type as "transaction type",
-i.internal_num as "item number",
-i.internal_name as 'item',
-t.quantity as 'quantity',
-i.internal_unit as 'unit',
-t.reason as 'reason',
-m.manager_name as 'manager'
+t.transaction_num,
+t.transaction_date,
+t.transaction_type,
+i.internal_num,
+i.internal_name,
+t.quantity,
+i.internal_unit,
+t.reason,
+m.manager_name,
+m.manager_num
 
 FROM Item as i JOIN InventoryTransaction as t
 ON i.internal_num = t.internal_num
--- sql/queryAll.sql
+++ sql/queryAll.sql
@@ -1,2 +1,57 @@
-SELECT * FROM ReceiveInfo;
+# Select Waste within the month of March of 2026
+SELECT * FROM WasteAdjustInfo
+WHERE transaction_type = 'WASTE'
+AND transaction_date >= "2026-03-01 00:00:00"
+AND transaction_date < "2026-04-01 00:00:00";
+
+# Select Adjustments and Waste done by Jeremy
+SELECT * FROM WasteAdjustInfo
+WHERE manager_name = 'Jeremy Dickinson';
+
+# get the total waste of each item
+SELECT 
+internal_num,
+internal_name,
+SUM(quantity) as total_waste
+FROM WasteAdjustInfo
+WHERE transaction_type = 'WASTE'
+GROUP BY internal_num, internal_name;
+SELECT * FROM InventoryTransaction;
+
+# select all invoiceline entries
+
+SELECT 
+i.invoice_num,
+i.vendor_pnum as product_number,
+p.vendor_pname as product_name,
+i.quantity
+FROM InvoiceLine AS i JOIN Product AS p
+ON i.vendor_pnum = p.vendor_pnum;
+
+
+# get inventory data
+
+SELECT 
+inv.internal_num, 
+it.internal_name,
+inv.quantity,
+it.internal_unit as unit
+FROM Inventory as inv LEFT JOIN Item as it
+ON inv.internal_num = it.internal_num;
+
+# get the total use of each item
+SELECT 
+internal_num,
+internal_name,
+SUM(quantity) as total_use
+FROM WasteAdjustInfo
+WHERE transaction_type = 'USE'
+GROUP BY internal_num, internal_name;
+
+# gets recent adjustments made to inventory
+SELECT *
+FROM WasteAdjustInfo
+WHERE transaction_type = 'ADJUST'
+ORDER BY transaction_date DESC;
+
 SELECT * FROM InventoryTransaction;
